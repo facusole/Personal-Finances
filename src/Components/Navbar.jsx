300f9cfea9f69362de0e245e6347df0e1b998eef
Facundo <facundojsole@gmail.com>
2022-07-13 13:21:11 -0300

    updates

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,26 @@
 import '../css/Navbar.css'
-import { Link } from 'react-router-dom'
+import { Link, useMatch, useResolvedPath } from 'react-router-dom'
 
 function Navbar() {
     return (
         <div className="nav-container">
-            <ul>
-                <li>
-                    <Link to="/incomes">Incomes</Link>
-                </li>
-                <li>
-                    <Link to="/expenses">Expenses</Link>
-                </li>
+            <ul>           
+                <CustomLink to="/incomes">Incomes</CustomLink>                
+                <CustomLink to="/expenses">Expenses</CustomLink>
             </ul>
         </div>
+    )
+}
+
+function CustomLink ({ to, children, ...props}) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({path: resolvedPath.pathname, end:true}) // end: true lo que hace es que todo el path sea igual y no parcialmente igual.
+    return (
+        <li className={ isActive ? 'active' : ''}>
+            <Link to={to} {...props}>
+                {children}
+            </Link>
+        </li>
     )
 }
 
